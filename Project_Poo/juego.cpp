b7e4730c1e55dc4084cc694bcdd6c6acf87fe895
v1c7or20 <victor.pena@utec.edu.pe>
2019-06-09 12:20:14 -0500

    contructor juego cpp
    constructor de juego cpp para

--- Project_Poo/juego.cpp
+++ Project_Poo/juego.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "juego.h"
+
+juego::juego(int num_jugadores):numero_jugadores(num_jugadores) {
+    dibujo=new dibujos(800,600,"Ludo",num_jugadores);
+    jugadores = new Jugador*[num_jugadores];
+    if(num_jugadores==2){
+        int **poscicicione=[]
+        *jugadores=new Jugador();
+    }
+    if(num_jugadores==3){
+
+    }
+    if(num_jugadores==4){
+
+    }
+}
