6012c3d71a578329e55dd4067a5658772ee3465e
v1c7or20 <victor.pena@utec.edu.pe>
2019-07-04 19:08:17 -0500

    ya hay inicio :"v
    -Falta hacerlo funcion
    -falta final de jugadores
    -falta aumentar jugabilidad
    -sigue faltando ella :"v aiuda

--- ProyectoPoo2/cmake-build-debug/cmake_install.cmake
+++ ProyectoPoo2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Renzo/Documents/GitHub/Proyecto-Poo2/ProyectoPoo2
+# Install script for directory: C:/Users/Usuario/Documents/GitHub/Proyecto-Poo2/ProyectoPoo2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Renzo/Documents/GitHub/Proyecto-Poo2/ProyectoPoo2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Usuario/Documents/GitHub/Proyecto-Poo2/ProyectoPoo2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
